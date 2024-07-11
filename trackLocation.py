4d3a37bf7209da593f070108a8ecef17e498a08a
ralvztech <info@ralvz.com>
2024-07-11 11:45:58 +0100

    Deploying track to github

--- trackLocation.py
+++ trackLocation.py
@@ -5,6 +5,26 @@ from phonenumbers import carrier
 import opencage
 from opencage.geocoder import OpenCageGeocode
 import folium
+from flask import Flask
+
+"""Starts a Flask Web Application"""
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return 'home.html'
+
+
+@app.route("/about")
+def about():
+    return 'about.html'
+
+
+@app.route("/location")
+def location():
+    return 'location.html'
 
 
 key = "ad6e7ced7e414f06b0d2351b9ab15c77" #Geocoder API Key needs to paste here "your key" 
@@ -32,3 +52,6 @@ folium.Marker([lat, lng], popup= location).add_to(my_map)
 my_map.save("location.html")
 
 print("location tracking completed")
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
